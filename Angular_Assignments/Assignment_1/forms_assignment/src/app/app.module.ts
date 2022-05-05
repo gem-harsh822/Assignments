1b6b595caf1222243008dcf7b46eaccb8ea20046
gem-harsh822 <Harsh.Gautam@Geminisolutions.com>
2022-05-05 16:04:12 +0530

    Added starting files for angular forms assignments,added bootstrap to the project,added navbar component , added 2 more components for template driven forms and reactive forms

---
+++ Angular_Assignments/Assignment_1/forms_assignment/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    TemplateDrivenFormComponent,
+    ReactiveFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
