1af9e007f95230a474c00a9bc0ffa62376d999d3
gem-harsh822 <Harsh.Gautam@Geminisolutions.com>
2022-05-12 10:09:51 +0530

    Created the json file for flight details

--- Angular_Assignments/Assignment1/src/app/flight-details-card/flight-details-card.component.ts
+++ Angular_Assignments/Assignment1/src/app/flight-details-card/flight-details-card.component.ts
@@ -6,9 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./flight-details-card.component.css']
 })
 export class FlightDetailsCardComponent implements OnInit {
-
+  flightDetails = [{
+    "FlightLogo":"'Angular_Assignments\Assignment1\src\assets\vistara_logo.png'",
+    "FlightName":"Vistara",
+    "From":"DEL New Delhi,India",
+    "To":"BOM Mumbai, India",
+    "DepartureTime":"17:45",
+    "TimeTaken":"2h 20m",
+    "ArrivalTime":"20:05",
+    "TicketPrice":"6,253",
+    "EmiPrice":"2,085"
+  },{
+    "FlightLogo":"'assets/vistara_logo.png'",
+    "FlightName":"Vistara",
+    "From":"DEL New Delhi,India",
+    "To":"BOM Mumbai, India",
+    "DepartureTime":"17:45",
+    "TimeTaken":"2h 20m",
+    "ArrivalTime":"20:05",
+    "TicketPrice":"6,253",
+    "EmiPrice":"2,085"
+  },
+  {
+    "FlightLogo":"'assets/vistara_logo.png'",
+    "FlightName":"Vistara",
+    "From":"DEL New Delhi,India",
+    "To":"BOM Mumbai, India",
+    "DepartureTime":"17:45",
+    "TimeTaken":"2h 20m",
+    "ArrivalTime":"20:05",
+    "TicketPrice":"6,253",
+    "EmiPrice":"2,085"
+  }]
   constructor() { }
-
+  
   ngOnInit(): void {
   }
 
